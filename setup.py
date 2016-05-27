9d13e3ece4bfc0b1b0f5a05582ea6c1839d9d9db
Piotr Gularski <piotr.gularski@gmail.com>
2016-05-27 17:00:31 +0100

    Keywords added to setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
@@ -33,4 +34,5 @@ setup(
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest', 'pytest-cov', 'mock'],
+    keywords='finite state machine automaton fsm hsm pda',
 )
